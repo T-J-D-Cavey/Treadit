2fac554b465d67a9a2c60bfbae473428b86532d9
Tim Cavey <caveyt@hotmail.co.uk>
2023-01-04 17:54:35 +0000

    Try and fix store error messages

--- src/Redux/postsSlice.js
+++ src/Redux/postsSlice.js
@@ -3,19 +3,38 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const getPosts = createAsyncThunk(
     'posts/getPosts',
     async (url) => {
-        return fetch(`https://www.reddit.com/${url}.json`)
-        .then(response => response.json())
-        .then(posts => {
-            console.log(posts);
-        })
+        const response = await fetch(`https://www.reddit.com/${url}.json`);
+        const data = await response.json();
+        const posts = data.data.children;
+            return posts;
     }
 )
+// Without awaits:
+// export const getPosts = createAsyncThunk(
+//     'posts/getPosts',
+//     async (url) => {
+//         return fetch(`https://www.reddit.com/${url}.json`)
+//         .then(response => response.json())
+//         .then(posts => {
+//             const listArray = posts.data.children;
+//             return listArray;
+//         })
+//     }
+// )
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState: {
         list: [],
-        status: null
+        status: null,
+        // creating a fake state to make a reducer to try and fix error:
+        testState: false
+    },
+    // creating a fake reducer to try and fix error:
+    reducers: {
+        changeTestState: (state) => {
+            state.testState = true;
+        }
     },
     extraReducers: {
         [getPosts.pending]: (state) => {
@@ -31,8 +50,12 @@ const postsSlice = createSlice({
     }
 });
 
-export const listSelector = (state) => {
-    return state.posts.list;
-}
+// export const listSelector = (state) => {
+//     return state.posts.list;
+// }
+
+// export const statusSelector = (state) => {
+//     return state.posts.status;
+// }
 
 export const {postsReducer} = postsSlice.reducer;
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,6 +1,13 @@
-import { listSelector } from '../../Redux/postsSlice';
+import { useSelector } from 'react-redux';
+
+// import { listSelector, statusSelector } from '../../Redux/postsSlice';
 
 export function Posts() {
+
+  // const list = useSelector(listSelector);
+  // const status = useSelector(statusSelector);
+
+  // console.log(list, status);
 
   return (
     <div>This will map over each post from an array of posts stored in state</div>
