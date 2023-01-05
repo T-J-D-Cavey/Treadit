996baa56abe4fd00953bcf6f58c11440a0a9b8a5
Tim Cavey <caveyt@hotmail.co.uk>
2023-01-05 22:23:32 +0000

    improve post component 2

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,14 +1,12 @@
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
 
-import { listSelector, statusSelector } from '../../Redux/postsSlice';
+import {listSelector} from '../../Redux/postsSlice';
+import redditIcon from '../../resources/images/redditIcon.svg';
+import videoIcon from '../../resources/images/videoIcon.svg';
 
 export function Posts() {
 
   const list = useSelector(listSelector);
-  const status = useSelector(statusSelector);
-
-
 
   console.log(list);
 
@@ -25,7 +23,6 @@ export function Posts() {
     let rawMinutes = a.getMinutes();
     let min;
     rawMinutes <10 ? min = `0${rawMinutes}` : min = rawMinutes;
-    // let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':';
     let time = `${date} ${month} ${year} ${hour}:${min}`;
     return time;
   }
@@ -39,13 +36,54 @@ export function Posts() {
       {list.map((element, index) => 
       <div key={index}  className='postContainer'>
         <div className='flexboxContainer postSection'>
-          <p>Posted by <span><a href={`https://www.reddit.com/user/${element.data.author}/`} target="_blank">{element.data.author}</a></span><span>    {timeConverter(element.data.created)}</span></p>
+          <p>Posted by <span><a href={`https://www.reddit.com/user/${element.data.author}/`} target="_blank">{element.data.author}</a></span>
+          <span>    {timeConverter(element.data.created)}</span></p>
         </div>
         <h3 className='postSection'><a href={`https://www.reddit.com${element.data.permalink}`} target="_blank">{element.data.title}</a></h3>
+        {element.data.post_hint === 'image' ? 
         
+        <a href={`https://www.reddit.com${element.data.permalink}`} target="_blank"><img src={element.data.url} alt='' className='postImage'/></a> 
+        : 
+        <a href={`https://www.reddit.com${element.data.permalink}`} target="_blank"><p>{element.data.selftext}</p></a> }
+
+        <div className='flexboxContainer postSection'>
+          {/* Will add a comments box svg here */}
+          <p>{element.data.num_comments === 1 ? 
+          `${element.data.num_comments} comment` 
+          : 
+          `${element.data.num_comments} comments`}<span> {element.data.ups} upvotes</span></p>
+        </div>
       </div> )}
     </div>
   )  
-  // 
+   
 
 }
+
+
+
+// return (
+//   <div className='flexItem postContainer'>
+//     {list.map((element, index) => 
+//     <div key={index}  className='postContainer'>
+//       <div className='flexboxContainer postSection'>
+//         <p>Posted by <span><a href={`https://www.reddit.com/user/${element.data.author}/`} target="_blank">{element.data.author}</a></span>
+//         <span>    {timeConverter(element.data.created)}</span></p>
+//       </div>
+//       <h3 className='postSection'><a href={`https://www.reddit.com${element.data.permalink}`} target="_blank">{element.data.title}</a></h3>
+//       {element.data.post_hint === 'image' ? 
+      
+//       <a href={`https://www.reddit.com${element.data.permalink}`} target="_blank"><img src={element.data.url} alt='' className='postImage'/></a> 
+//       : 
+//       <a href={`https://www.reddit.com${element.data.permalink}`} target="_blank"><p>{element.data.selftext}</p></a> }
+
+//       <div className='flexboxContainer postSection'>
+//         {/* Will add a comments box svg here */}
+//         <p>{element.data.num_comments === 1 ? 
+//         `${element.data.num_comments} comment` 
+//         : 
+//         `${element.data.num_comments} comments`}<span> {element.data.ups} upvotes</span></p>
+//       </div>
+//     </div> )}
+//   </div>
+// )
