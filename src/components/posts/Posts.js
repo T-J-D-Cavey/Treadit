18b4e4f19132262216ada0c47a1f87b1d9cfe50f
Tim Cavey <caveyt@hotmail.co.uk>
2023-01-05 20:22:07 +0000

    improve post component

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -11,19 +11,39 @@ export function Posts() {
 
 
   console.log(list);
+
+// Converts UTC/UNIX timestamp into a date and time:
+  const timeConverter = (UNIX_timestamp) => {
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let rawHours = a.getHours();
+    let hour;
+    rawHours <10 ? hour = `0${rawHours}` : hour = rawHours;
+    let rawMinutes = a.getMinutes();
+    let min;
+    rawMinutes <10 ? min = `0${rawMinutes}` : min = rawMinutes;
+    // let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':';
+    let time = `${date} ${month} ${year} ${hour}:${min}`;
+    return time;
+  }
+
   // I have hardcoded a limit of 20 in the postsSlice. 
   // This could be made dynamic with filter options and new state being added, 
   // and/or a 'next page' button added to the bottom of the list.
-
-
   
   return (
     <div className='flexItem postContainer'>
       {list.map((element, index) => 
-      <a href={`https://www.reddit.com${element.data.permalink}`} key={index} target="_blank" className='postContainer'>
-        <h3>{element.data.title}</h3>
-        <p>Posted by <span>{element.data.author}</span></p>
-      </a> )}
+      <div key={index}  className='postContainer'>
+        <div className='flexboxContainer postSection'>
+          <p>Posted by <span><a href={`https://www.reddit.com/user/${element.data.author}/`} target="_blank">{element.data.author}</a></span><span>    {timeConverter(element.data.created)}</span></p>
+        </div>
+        <h3 className='postSection'><a href={`https://www.reddit.com${element.data.permalink}`} target="_blank">{element.data.title}</a></h3>
+        
+      </div> )}
     </div>
   )  
   // 
