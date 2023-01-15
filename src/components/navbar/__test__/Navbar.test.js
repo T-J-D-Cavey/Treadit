f90fa8a791d56c91a51d089a613a64eee16f4fa3
Tim Cavey <caveyt@hotmail.co.uk>
2023-01-15 17:50:09 +0000

    add more tests for NavBar component

--- src/components/navbar/__test__/Navbar.test.js
+++ src/components/navbar/__test__/Navbar.test.js
@@ -3,22 +3,52 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import {postsReducer} from '../Redux/postsSlice';
-import {store} from '../Redux/store';
-import {Navbar} from '../components/navbar/Navbar';
+import {postsReducer} from '../../../Redux/postsSlice';
+import {store} from '../../../Redux/store';
+import {Navbar} from '../Navbar';
 
 
 describe('Navbar component tests:', () => {
 
-
-
+// One approach, destructuring getByRole method from render function:
   test('renders the search button:', () => {
     const {getByRole} = render(
       <Provider store={store}>
         <Navbar />
       </Provider>
-    )
+    );
     expect(getByRole('button', { name: /search/i })).toBeInTheDocument()
+  });
+
+  // Equivalent test to the one above but using screen object: 
+  test('renders the filter button:', () => {
+    render(
+      <Provider store={store}>
+        <Navbar />
+      </Provider>
+    );
+    const filterButton = screen.getByRole('button', { name: /filter/i })
+    expect(filterButton).toBeInTheDocument()
+  });
+// The test below was failing till it was made async / await:
+  test('renders an image element with an alt text of logo', async () => {
+    render(
+      <Provider store={store}>
+        <Navbar />
+      </Provider>
+    );
+    const logoIcon = await screen.findByAltText('logo');
+    expect(logoIcon).toBeInTheDocument();
+  });
+
+  test('renders 3 image elements in the NavBar component', async () => {
+    render(
+      <Provider store={store}>
+        <Navbar />
+      </Provider>
+    );
+    const imageElements = await screen.findAllByRole('img');
+    expect(imageElements).toHaveLength(3);
   })
 
 });
