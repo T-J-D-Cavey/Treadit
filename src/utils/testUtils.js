a123b58c45cf634e2716372215bd797a711e8c55
Tim Cavey <caveyt@hotmail.co.uk>
2023-01-18 19:08:38 +0000

    Configure renderWithProviders for integration tests with redux state

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import postsReducer from '../Redux/postsSlice';
+import {postsReducer} from '../Redux/postsSlice';
+
+// Create the root reducer separately so we can extract the RootState type
+const rootReducer = combineReducers({
+  posts: postsReducer
+});
+
+export const setupStore = preloadedState => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState
+  })
+};
 
 export function renderWithProviders(
   ui,
   {
     preloadedState = {},
     // Automatically create a store instance if no store was passed in
-    store = configureStore({ reducer: { posts: postsReducer }, preloadedState }),
+    store = setupStore(preloadedState),
     ...renderOptions
   } = {}
 ) {
