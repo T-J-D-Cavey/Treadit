5e6a24e4ed8663ebec17d5bed5bc31fbff40379d
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-05-15 17:10:07 +0100

    set up redux and async state for component testing

--- cypress/component/Mainpage.cy.js
+++ cypress/component/Mainpage.cy.js
@@ -2,6 +2,14 @@ import {Mainpage} from '../../src/components/mainpage/Mainpage';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import {postsReducer} from '../../src/Redux/postsSlice';
+import { FailedToLoad } from '../../src/components/mainpage/FailedToLoad';
+
+import {useSelector, useDispatch, useContext} from 'react-redux';
+import {
+    getPosts, subredditSelector, listingSelector,
+    limitSelector, timeframeSelector, searchTermSelector, 
+    searchbarSelector, filterSelector, setSearchbar, setFilter
+} from '../../src/Redux/postsSlice';
 
 
 describe('Loading.cy.js', () => {
@@ -9,14 +17,22 @@ describe('Loading.cy.js', () => {
         reducer: {
             posts: postsReducer
         }
-       
     })
+    const URL = 'https://www.reddit.com/r/hiking/hot.json?limit=50&t=week';
+    
     it('playground', () => {
+        cy.intercept('GET', URL, {
+            fixture: "mockResponse.json"
+        }).as('getPosts')
         cy.mount(
             <Provider store={store}>
                 <Mainpage  />
             </Provider>
-        )
+            // <FailedToLoad />
+            )
+            // const dispatch = useDispatch();
+            // dispatch(getPosts(URL));
+        store.dispatch(getPosts())
       // I need to write tests here:
   })
 })
