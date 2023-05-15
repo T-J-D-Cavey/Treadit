1f17d19894ea6d3c9b939c2e848a83db3705af9c
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-05-15 22:41:31 +0100

    write more component tests

--- cypress/component/FilterForm.cy.js
+++ cypress/component/FilterForm.cy.js
@@ -4,14 +4,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { postsReducer } from '../../src/Redux/postsSlice';
 import { getPosts } from '../../src/Redux/postsSlice';
 
-
-
     describe('FilterForm.cy.js', () => {
         const store = configureStore({
             reducer: {
                 posts: postsReducer
             }
         })
+        // to give insight into initial state. This is not the app's store so changes in state won't be picked up:
+        const state = store.getState();
         const URL = 'https://www.reddit.com/r/hiking/hot.json?limit=50&t=week';
         it('playground', () => {
             cy.intercept('GET', URL, {
@@ -20,8 +20,16 @@ import { getPosts } from '../../src/Redux/postsSlice';
             cy.mount(
                 <Provider store={store}>
                     <FilterForm />
-                </Provider>)
+                </Provider>
+                )
             store.dispatch(getPosts(URL))
-        // I need to write tests here:
+            // Fill in form values and tests input now selected:
+            cy.get('#subreddit').select('r/Adventures').should('have.value', 'r/Adventures');
+            cy.get('#listing').select('hot').should('have.value', 'hot');
+            cy.get('#limit').select('10').should('have.value', '10');
+            cy.get('#timeframe').select('day').should('have.value', 'day');
+
+            // Submit the form
+            // cy.get('.filterForm').submit();
     })
   })
