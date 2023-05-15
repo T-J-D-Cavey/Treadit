958aa7b796db8084328b16bb31f81b50f7915264
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-05-15 21:43:23 +0100

    finish FailedToLoad component tests

--- cypress/component/FailedToLoad.cy.js
+++ cypress/component/FailedToLoad.cy.js
@@ -3,6 +3,9 @@ import {FailedToLoad} from '../../src/components/mainpage/FailedToLoad';
 describe('Loading.cy.js', () => {
   it('playground', () => {
       cy.mount(<FailedToLoad />)
-      // I need to write tests here:
+      cy.contains('h3', 'Failed to load content.').should('be.visible');
+      cy.contains('p', 'Refresh your browser or check your internet connection.').should('be.visible');
+      cy.get('.flex').find('[class="spinnerContainer"]').should('be.visible');
+      cy.get('.flex').find('[class="spinnerContainer"]').should('have.css', 'animation').and('match', /spinning/);
   })
 })
