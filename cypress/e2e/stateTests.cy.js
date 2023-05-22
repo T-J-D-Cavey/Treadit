c4caf347edae700090d78530d1335737476bc55c
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-05-22 22:36:15 +0100

    complete all state tests in E2E spec

--- cypress/e2e/stateTests.cy.js
+++ cypress/e2e/stateTests.cy.js
@@ -34,11 +34,15 @@ describe('State / store tests', () => {
         cy.get('.filterForm').submit();
 
         cy.window().its('store').invoke('getState').then((state) => {
-            expect(state.posts.status).to.equal('loading');
             expect(state.posts.subreddit).to.equal('r/First_Aid');
             expect(state.posts.listing).to.equal('new');
             expect(state.posts.limit).to.equal('10');
             expect(state.posts.timeframe).to.equal('month');
+            expect(state.posts.filter).to.be.true;
+            expect(state.posts.searchbar).to.be.false;
+            expect(state.posts.status).to.satisfy((status) => {
+                return status === 'loading' || status === 'success';
+              });
         })
     })
 
@@ -50,12 +54,29 @@ describe('State / store tests', () => {
         cy.get('.filterForm').submit();
 
         cy.window().its('store').invoke('getState').then((state) => {
-            expect(state.posts.status).to.equal('loading');
             expect(state.posts.searchTerm).to.equal('testing%20this%20with%20Cypress');
             expect(state.posts.limit).to.equal('100');
             expect(state.posts.timeframe).to.equal('year');
+            expect(state.posts.filter).to.be.false;
+            expect(state.posts.searchbar).to.be.true;
+            expect(state.posts.status).to.satisfy((status) => {
+                return status === 'loading' || status === 'success';
+              });
         })
     })
 
+    it('should send an HTTP request with the correct params', () => {
+        const URL = 'https://www.reddit.com/r/hiking/search.json?limit=100&t=year&q=testing%20this%20with%20Tim';
+        cy.intercept('GET', URL, (res) => {
+            // Intercepts the GET request only if it exactly matches the URL expected
+            expect(res.url).to.equal(URL);
+        }).as('getPosts')
 
+        cy.getSearchButton().click();
+        cy.get('input[placeholder="search..."]').type('testing this with Tim');
+        cy.get('select[id="limit"]').select('100');
+        cy.get('select[id="timeframe"]').select('year');
+        cy.get('.filterForm').submit();
+        cy.wait('@getPosts')
+    })
 })
